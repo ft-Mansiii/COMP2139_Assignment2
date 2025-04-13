02e23e1463377bbd339a6dafe308d856adc3758c
ft-Mansiii <mansi4@georgebrown.ca>
2025-04-12 22:21:31 -0400

    Delete RiderProjects/Mansi/Assignment01 directory

--- RiderProjects/Mansi/Assignment01/Assignment_01/Areas/Management/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Assignment01.Areas.Management.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
